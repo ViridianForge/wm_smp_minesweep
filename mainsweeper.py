5882281696dc302e88d8da40c018d19795936882
Wayne Manselle <viridianforge@gmail.com>
2018-10-06 21:28:50 -0700

    Barely functional
    Getting some printing working. Needs to be cleaned up to actually display properly. Basic logic to pick difficulty. Need to add actual mine placement and gameplay.

--- mainsweeper.py
+++ mainsweeper.py
@@ -5,33 +5,66 @@ Simple CLI Minesweeper Game
 Made for exercise purposes
 '''
 
-'''
-Set up game status
-'''
-def conf_game():
-    return
+import os
+import array
+import random
+import math
 
-'''
-Display Game to Command Line
-'''
-def disp_game():
-    return
+class Minesweeper:
+    def __init__(self):
+        self._viewLayer = None
+        self._mineLayer = None
+        self._gameon = True
+        self._difficulty = 0
+        self._configs = [64,100,576]
 
-'''
-Get player's next moove
-'''
-def get_move():
-    return
+    '''
+    Set up game status
+    '''
+    def conf(self):
+        #Request input to configure game
+        while(self._difficulty == 0):
+            print("What game difficulty would you like? \n 1. Beginner -- (8x8) 10 mines \n 2. Intermediate -- (10x10) 40 mines \n 3. Expert -- (24x24) 99 mines")
+            pot_diff = input("Respond with 1 for Beginner, 2 for Intermediate, or 3 for Expert")
+            if pot_diff in ["1", "2", "3"]:
+                self._difficulty = int(pot_diff) #64, 100, 576
+            else:
+                print("That was an invalid response.")
+                os.system("clear")
+        
+        #Initialize the gameboard
+        self._viewLayer = array.array('i',(0 for i in range(0,self._configs[self._difficulty])))
+        self._mineLayer = array.array('i',(0 for i in range(0,len(self._viewLayer))))
+
+    def active(self):
+        return self._gameon
+
+    '''
+    Display Game to Command Line
+    '''
+    def disp_game(self):
+        os.system("clear")
+        for i in range(0,len(self._viewLayer), int(math.sqrt(len(self._viewLayer)))):
+            print(str(self._viewLayer[i:i+int(math.sqrt(len(self._viewLayer)))]) + "\n")
+        return
+
+    '''
+    Get player's next moove
+    '''
+    def get_move(self):
+        input("Make your move.")
+        return
 
 '''
 Main Method
 '''
 def main():
-    conf_game()
-    while(self._gameon):
-        disp_game()
-        get_move()
+    print("Welcome to Minesweeper!")
+    game = Minesweeper()
+    game.conf()
+    while(game.active):
+        game.disp_game()
+        game.get_move()
 
 
-if __name__ == "Main":
-    main()
+if __name__ == "__main__" : main()
