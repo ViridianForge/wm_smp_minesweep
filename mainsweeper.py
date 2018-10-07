5957814e276dea7f06fc0469779604e33abcf6f2
Wayne Manselle <viridianforge@gmail.com>
2018-10-06 19:55:48 -0700

    First commit
    Just some practice exercise

---
+++ mainsweeper.py
@@ -0,0 +1,37 @@
+'''
+Simple CLI Minesweeper Game
+
+@author Wayne Manselle
+Made for exercise purposes
+'''
+
+'''
+Set up game status
+'''
+def conf_game():
+    return
+
+'''
+Display Game to Command Line
+'''
+def disp_game():
+    return
+
+'''
+Get player's next moove
+'''
+def get_move():
+    return
+
+'''
+Main Method
+'''
+def main():
+    conf_game()
+    while(self._gameon):
+        disp_game()
+        get_move()
+
+
+if __name__ == "Main":
+    main()
