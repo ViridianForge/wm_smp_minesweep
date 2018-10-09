482a2c068fe9892f0b33a88d536cca5b0363f675
Wayne Manselle <viridianforge@gmail.com>
2018-10-08 21:26:47 -0700

    Update mainsweeper.py
    Adding more ultra rough logic.

--- mainsweeper.py
+++ mainsweeper.py
@@ -25,7 +25,7 @@ class Minesweeper:
         #Request input to configure game
         while(self._difficulty == 0):
             print("What game difficulty would you like? \n 1. Beginner -- (8x8) 10 mines \n 2. Intermediate -- (10x10) 40 mines \n 3. Expert -- (24x24) 99 mines")
-            pot_diff = input("Respond with 1 for Beginner, 2 for Intermediate, or 3 for Expert")
+            pot_diff = input("Respond with 1 for Beginner, 2 for Intermediate, or 3 for Expert. ")
             if pot_diff in ["1", "2", "3"]:
                 self._difficulty = int(pot_diff) #64, 100, 576
             else:
@@ -33,9 +33,10 @@ class Minesweeper:
                 os.system("clear")
         
         #Initialize the gameboard
-        self._viewLayer = array.array('i',(0 for i in range(0,self._configs[self._difficulty])))
+        self._viewLayer = array.array('i',(0 for i in range(0,self._configs[(self._difficulty)-1])))
         self._mineLayer = array.array('i',(0 for i in range(0,len(self._viewLayer))))
 
+    @property
     def active(self):
         return self._gameon
 
@@ -52,8 +53,38 @@ class Minesweeper:
     Get player's next moove
     '''
     def get_move(self):
-        input("Make your move.")
+        print("Would you like to? \n 1. Scan an area? \n 2. Flag a mine? \n 3. Sweep an area? \n 4. Quit in Disgust.")
+        move = None
+        
+        while(move not in ["1","2","3","4"]):
+            move = input("Respond with 1, 2, 3, or 4. ")
+        
+        if move=="1":
+            self._scan_move()
+        elif move=="2":
+            self._flag_move()
+        elif move=="3":
+            self._sweep_move()
+        else:
+            self._quit_game()
+        
         return
+
+    def _scan_move(self):
+        print("Scan Logic Goes here")
+        return
+    
+    def _flag_move(self):
+        print("Flag Logic Goes here")
+        return
+
+    def _sweep_move(self):
+        print("Sweep Logic Goes here")
+        return
+
+    def _quit_game(self):
+        print("Sometimes surrender is the wise decision. Good day.")
+        self._gameon = False
 
 '''
 Main Method
